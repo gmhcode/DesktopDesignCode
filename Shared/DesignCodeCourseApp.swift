d6dbb9c101184355de1b9603751501c3096a7183
Greg Hughes <dcgg_101@hotmail.com>
2021-02-04 16:58:06 -0700

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Greg Hughes on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
