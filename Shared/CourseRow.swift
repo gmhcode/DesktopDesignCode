b102848245bd40de0137260784debb1c32cb91d7
Greg Hughes <dcgg_101@hotmail.com>
2021-02-04 17:52:51 -0700

    first commit

---
+++ Shared/CourseRow.swift
@@ -0,0 +1,20 @@
+//
+//  CourseRow.swift
+//  DesignCodeCourse
+//
+//  Created by Greg Hughes on 2/4/21.
+//
+
+import SwiftUI
+
+struct CourseRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseRow()
+    }
+}
---
+++ Shared/CoursesView.swift
@@ -0,0 +1,20 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Greg Hughes on 2/4/21.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
