9a1baacfca510b7e978f4bfcd7c9ffa15ddc3aba
Greg Hughes <dcgg_101@hotmail.com>
2021-02-04 18:00:47 -0700

    added ContentView, CourseRow, CoursesView, and Sidebar

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct CoursesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
     }
 }
 
