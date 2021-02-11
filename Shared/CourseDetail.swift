8b6f09b8d0b027aa76b2b7d8975b035c546a7dd2
Greg Hughes <dcgg_101@hotmail.com>
2021-02-10 17:23:23 -0700

    added better mac functionality for courseDetail and CoursesView

--- Shared/CourseDetail.swift
+++ Shared/CourseDetail.swift
@@ -11,10 +11,25 @@ struct CourseDetail: View {
     var course: Course = courses[0]
     var namespace: Namespace.ID
     
+    #if os(iOS)
+    var cornerRadius: CGFloat = 22
+    #else
+    var cornerRadius: CGFloat = 0
+    #endif
+    
     var body: some View {
+        #if os(iOS)
+        content
+            .edgesIgnoringSafeArea(.all)
+        #else
+        content
+        #endif
+        
+    }
+    var content: some View {
         VStack {
             ScrollView {
-                CourseItem(course: course)
+                CourseItem(course: course, cornerRadius: 0)
                     .matchedGeometryEffect(id: course.id, in: namespace)
                     .frame(height: 300)
                 VStack {
@@ -22,15 +37,15 @@ struct CourseDetail: View {
                         CourseRow(item: item)
                         Divider()
                     }
-                    
                 }
                 .padding()
             }
         }
         
         .background(Color("Background 1"))
-        .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+        .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
         .matchedGeometryEffect(id: "container\(course.id)", in: namespace)
+        
         //                .transition(
         //                    .asymmetric(
         //                        insertion: AnyTransition
@@ -40,8 +55,6 @@ struct CourseDetail: View {
         //                            .opacity
         //                            .animation(Animation.spring()))
         //                )
-        
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -16,51 +16,71 @@ struct CoursesView: View {
     
     var body: some View {
         ZStack {
-            ScrollView {
-                //adaptive layout fits as many cards as possible with a min width of 160
-                LazyVGrid(
-                    columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
-                    spacing: 16) {
-                    ForEach(courses) { item in
-                        VStack {
-                            CourseItem(course: item)
-                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                                .frame(height: 200)
-                                .onTapGesture {
-                                    withAnimation(.spring(response: 0.4, dampingFraction: 0.8, blendDuration: 0)) {
-                                        show.toggle()
-                                        selectedItem = item
-                                        isDisabled = true
-                                    }
-                                }
-                                .disabled(isDisabled)
-                        }
-                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
-                    }
-                }
-                .padding(16)
-                .frame(maxWidth: .infinity)
-            }
-            //keeps the content aboce the other cards
-            .zIndex(1)
-            //            .edgesIgnoringSafeArea(.all)
-            if selectedItem != nil {
-                ZStack(alignment: .topTrailing) {
-                    CourseDetail(course: selectedItem!, namespace: namespace)
-                    CloseButton()
-                        .padding(.trailing, 16)
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                show.toggle()
-                                selectedItem = nil
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                                    isDisabled = false
+            #if os(iOS)
+            content
+                .navigationBarHidden(true)
+            fullContent
+                .background(VisualEffectBlur(blurStyle: .systemUltraThinMaterial).edgesIgnoringSafeArea(.all))
+            #else
+            content
+            fullContent
+            .background(VisualEffectBlur().edgesIgnoringSafeArea(.all))
+            #endif
+        }
+        .navigationTitle(Text("Courses"))
+    }
+    
+    var content: some View {
+        ScrollView {
+            //adaptive layout fits as many cards as possible with a min width of 160
+            LazyVGrid(
+                columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+                spacing: 16) {
+                ForEach(courses) { item in
+                    VStack {
+                        CourseItem(course: item)
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(height: 200)
+                            .onTapGesture {
+                                withAnimation(.spring(response: 0.4, dampingFraction: 0.8, blendDuration: 0)) {
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisabled = true
                                 }
                             }
-                        }
+                            .disabled(isDisabled)
+                    }
+                    .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                 }
-                .zIndex(2)
             }
+            .padding(16)
+            .frame(maxWidth: .infinity)
+        }
+        //keeps the content aboce the other cards
+        .zIndex(1)
+        //            .edgesIgnoringSafeArea(.all)
+    }
+    @ViewBuilder
+    var fullContent: some View {
+        if selectedItem != nil {
+            ZStack(alignment: .topTrailing) {
+                CourseDetail(course: selectedItem!, namespace: namespace)
+                CloseButton()
+                    .padding(16)
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            show.toggle()
+                            selectedItem = nil
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                isDisabled = false
+                            }
+                        }
+                    }
+            }
+            .zIndex(2)
+            .frame(maxWidth: 712)
+            .frame(maxWidth:.infinity)
+            
         }
     }
 }
